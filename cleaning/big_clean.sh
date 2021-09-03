9b09016669bbd8b5dacab2b10ef03114a07b215b
Thomas <dahlg136@morris.umn.edu>
2021-09-02 19:50:35 -0500

    Fix spellcheck mistakes
    After running spellcheck we made the fixes it wanted us to make for our code to be better.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -7,11 +7,11 @@ here=$(pwd)
 TMPDIR=$(mktemp -d) || exit 1
 
 #Copies over the files of the inputed directory into the temp directory
-tar -zxvf $1 -C $TMPDIR
+tar -zxvf "$1" -C "$TMPDIR"
 
 #Finds and deletes and file containing the line 'DELETE ME!'
-grep -rl "DELETE ME!" $TMPDIR | xargs rm -f
+grep -rl "DELETE ME!" "$TMPDIR" | xargs rm -f
 
-cd $TMPDIR
+cd "$TMPDIR" || exit
 #Copies the files over into a new archive
-tar -czvf /$here/cleaned_$1 "$(basename $1 .tgz)"
+tar -czvf /"$here"/cleaned_"$1" "$(basename "$1" .tgz)"
