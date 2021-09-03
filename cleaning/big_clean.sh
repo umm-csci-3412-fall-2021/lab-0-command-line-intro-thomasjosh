3da8c7cf9f1d304ff9398e5ea36736922fd1c957
Thomas <dahlg136@morris.umn.edu>
2021-09-02 19:39:36 -0500

    Make it so almost every test passes for cleaning
    Push to save progress of every test passing except the last where we need to figure out how to get the base name.

--- cleaning/big_clean.sh
+++ cleaning/big_clean.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-
+here=$(pwd)
 TMPDIR=$(mktemp -d) || exit 1
 
 tar -zxvf $1 -C $TMPDIR
 
-grep -r "DELETE ME!" $TMPDIR | xargs rm
+grep -rl "DELETE ME!" $TMPDIR | xargs rm -f
 
+cd $TMPDIR
+base='basename $1'
+tar -czvf /$here/cleaned_$1 little_dir
